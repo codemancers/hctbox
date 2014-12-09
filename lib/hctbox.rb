dd9228bd213816b8d0cbf6f9281b8e965057ecca
Yuva <yuva@codemancers.com>
2014-12-09 18:05:47 +0530

    add a module accessor to require or ignore assets

--- lib/hctbox.rb
+++ lib/hctbox.rb
@@ -10,6 +10,14 @@ module Hctbox
   self.stylesheets_css_path = File.expand_path('../../app/assets/stylesheets', __FILE__)
 
 
+  # attribute which includes or excludes assets. Right now, gem requires
+  # users to include hctbox.js and hctbox.css in main_app manifest
+  # files. Setting this flag to false will prevent hctbox to be required
+  # in asset pipeline.
+  mattr_accessor :require_assets
+  self.require_assets = Rails.env.development?
+
+
   # can be used to configure module attributes like stylesheets css path.
   def self.config()
     raise 'no block given' unless block_given?
